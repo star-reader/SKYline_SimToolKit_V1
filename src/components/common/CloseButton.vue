76d7e996483297416d039d76b9d75a3d3e3bf158
star-reader <3084895416@qq.com>
2024-08-07 17:40:28 +0800

    更新关闭按钮

---
+++ src/components/common/CloseButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="close-button" :button-type=props.isAeroGlass>
+            <close theme="outline" size="14" fill="#ffffff"/>
+        </div>
+</template>
+
+<script lang='ts' setup>
+import { Close } from '@icon-park/vue-next'
+
+interface IProps {
+    isAeroGlass: boolean
+}
+const props = defineProps<IProps>()
+
+</script>
+
+<style lang='less' scoped>
+
+</style>
